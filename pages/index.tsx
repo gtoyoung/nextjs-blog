7332a3d8a0cc9eb20f68ee13b9553f9c59b23bc0
ban <gtoyoung@naver.com>
2022-02-10 10:25:00 +0900

    webhook 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default class Index extends Component {
             <h1>Home Page</h1>
             <div className="paper container margin-bottom-large">
               <h4>Dovb`s Blog Notice</h4>
-              <p>블로그 개편 중</p>
+              <p>블로그 개편 진행중</p>
               <p>
                 기존 Gatsby + Contentful로 구성되어있던 블로그를 Nextjs +
                 Contentful로 개편을 진행중입니다.
