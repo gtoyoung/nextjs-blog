ddcf690d0fea7a62d9149ef1c7b9d257724c8910
ban <gtoyoung@naver.com>
2022-02-28 10:56:58 +0900

    나무생성기 배경 추가

--- public/trees/src/js/app.js
+++ public/trees/src/js/app.js
@@ -2,12 +2,59 @@ import { Tree } from "./tree.js";
 
 class App {
   constructor() {
+    // 첫번째 레이어
+    this.backCanvas = document.createElement("canvas");
+    this.backCanvas.classList.add("snowBack");
+    this.backCanvas.style =
+      "position: absolute; top: 0; left: 0; z-index: 0; display: none;";
+    document.body.appendChild(this.backCanvas);
+    // 두번째 레이어
+    this.backCanvas2 = document.createElement("canvas");
+    this.backCanvas2.classList.add("rainBack");
+    this.backCanvas2.style =
+      "position: absolute; top: 0; left: 0; z-index: 0; display: none;";
+    document.body.appendChild(this.backCanvas2);
+
+    // 세번째 레이어
     this.canvas = document.createElement("canvas");
+    this.canvas.style = "position: absolute; top: 0; left: 0; z-index: 1;";
     document.body.appendChild(this.canvas);
 
     this.ctx = this.canvas.getContext("2d");
+    this.backCtx = this.backCanvas.getContext("2d");
+    this.backCtx2 = this.backCanvas2.getContext("2d");
     this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
     this.depth = 0;
+    this.angle = 0;
+    this.mp = 50;
+    this.particles = [];
+    // snowflake paritcles
+    for (var i = 0; i < this.mp; i++) {
+      this.particles.push({
+        x: Math.random() * window.innerWidth, //x-coordinate
+        y: Math.random() * window.innerHeight, //y-coordinate
+        r: Math.random() * 4 + 1, //radius
+        d: Math.random() * this.mp, //density
+      });
+    }
+
+    this.rains = [];
+    var maxParts = 1000;
+    for (var i = 0; i < maxParts; i++) {
+      this.rains.push({
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+        l: Math.random() * 1,
+        xs: -4 + Math.random() * 4 + 2,
+        ys: Math.random() * 10 + 10,
+      });
+    }
+
+    this.rainParticles = [];
+    for (var i = 0; i < maxParts; i++) {
+      this.rainParticles[i] = this.rains[i];
+    }
+
     window.addEventListener("resize", this.resize.bind(this), false);
 
     if (this.isMobile()) {
@@ -18,6 +65,9 @@ class App {
       window.addEventListener("mousedown", this.mouseDown.bind(this), false);
       window.addEventListener("mouseup", this.mouseUp.bind(this), false);
     }
+
+    setInterval(this.snowDraw.bind(this), 33);
+    setInterval(this.rainDraw.bind(this), 30);
     this.resize();
     // this.setBtn();
   }
@@ -105,6 +155,7 @@ class App {
 
     this.canvas.width = this.stageWidth * this.pixelRatio;
     this.canvas.height = this.stageHeight * this.pixelRatio;
+
     this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
     this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
@@ -114,6 +165,105 @@ class App {
     const { clientX } = event;
     if (event.target.className !== "material-icons") {
       new Tree(this.ctx, clientX, this.stageHeight, this.day, this.depth);
+    }
+  }
+
+  // 눈 그리기
+  snowDraw() {
+    var w = document.body.clientWidth;
+    var h = document.body.clientHeight;
+
+    this.backCanvas.width = w * this.pixelRatio;
+    this.backCanvas.height = h * this.pixelRatio;
+
+    this.backCtx.scale(this.pixelRatio, this.pixelRatio);
+    this.backCtx.clearRect(0, 0, w, h);
+    this.backCtx.fillStyle = "rgba(255, 255, 255, 0.8)";
+    this.backCtx.beginPath();
+
+    for (var i = 0; i < this.mp; i++) {
+      var p = this.particles[i];
+      this.backCtx.moveTo(p.x, p.y);
+      this.backCtx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
+    }
+    this.backCtx.fill();
+    this.update(w, h);
+  }
+
+  update(w, h) {
+    this.angle += 0.01;
+    for (var i = 0; i < this.mp; i++) {
+      var p = this.particles[i];
+      //Updating X and Y coordinates
+      //We will add 1 to the cos function to prevent negative values
+      //which will result in the flakes to move upwards
+      //Every particle has its own density which can be used to make the downward movement different for each flake
+      //Let's make it more random by adding in the radius
+      p.y += Math.cos(this.angle + p.d) + 1 + p.r / 2;
+      p.x += Math.sin(this.angle) * 2;
+      if (p.x > w + 5 || p.x < -5 || p.y > h) {
+        if (i % 3 > 0) {
+          this.particles[i] = {
+            x: Math.random() * w,
+            y: -10,
+            r: p.r,
+            d: p.d,
+          };
+        } else {
+          if (Math.sin(this.angle) > 0) {
+            this.particles[i] = {
+              x: -5,
+              y: Math.random() * h,
+              r: p.r,
+              d: p.d,
+            };
+          } else {
+            this.particles[i] = {
+              x: w + 5,
+              y: Math.random() * h,
+              r: p.r,
+              d: p.d,
+            };
+          }
+        }
+      }
+    }
+  }
+
+  rainDraw() {
+    var w = document.body.clientWidth;
+    var h = document.body.clientHeight;
+
+    this.backCanvas2.width = w * this.pixelRatio;
+    this.backCanvas2.height = h * this.pixelRatio;
+
+    this.backCtx2.strokeStyle = "rgba(174,194,224,0.5)";
+    this.backCtx2.lineWidth = 2;
+    this.backCtx2.lineCap = "round";
+
+    this.backCtx2.scale(this.pixelRatio, this.pixelRatio);
+    this.backCtx2.clearRect(0, 0, w, h);
+
+    for (var i = 0; i < this.rainParticles.length; i++) {
+      var p = this.rainParticles[i];
+      this.backCtx2.beginPath();
+      this.backCtx2.moveTo(p.x, p.y);
+      this.backCtx2.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+      this.backCtx2.stroke();
+    }
+
+    this.move(w, h);
+  }
+
+  move(w, h) {
+    for (var i = 0; i < this.rainParticles.length; i++) {
+      var p = this.rainParticles[i];
+      p.x += p.xs;
+      p.y += p.ys;
+      if (p.x > w || p.y > h) {
+        p.x = Math.random() * w;
+        p.y = -20;
+      }
     }
   }
 }
