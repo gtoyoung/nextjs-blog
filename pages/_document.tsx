4aa5378911c4fe90ad6c3d1930b7e413cc8ec528
dovb <gtoyoung@naver.com>
2022-03-16 10:56:53 +0900

    상담 채널 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -71,6 +71,50 @@ export default class CustomDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+              (function() {
+                var w = window;
+                if (w.ChannelIO) {
+                  return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+                }
+                var ch = function() {
+                  ch.c(arguments);
+                };
+                ch.q = [];
+                ch.c = function(args) {
+                  ch.q.push(args);
+                };
+                w.ChannelIO = ch;
+                function l() {
+                  if (w.ChannelIOInitialized) {
+                    return;
+                  }
+                  w.ChannelIOInitialized = true;
+                  var s = document.createElement('script');
+                  s.type = 'text/javascript';
+                  s.async = true;
+                  s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+                  s.charset = 'UTF-8';
+                  var x = document.getElementsByTagName('script')[0];
+                  x.parentNode.insertBefore(s, x);
+                }
+                if (document.readyState === 'complete') {
+                  l();
+                } else if (window.attachEvent) {
+                  window.attachEvent('onload', l);
+                } else {
+                  window.addEventListener('DOMContentLoaded', l, false);
+                  window.addEventListener('load', l, false);
+                }
+              })();
+              ChannelIO('boot', {
+                "pluginKey": "4fa936b8-48ea-47b6-b193-1a6cc53a274d"
+              });
+            `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               if(!wcs_add) var wcs_add = {};
               wcs_add["wa"] = "12864c3367708c0";
               if(window.wcs) {
@@ -79,7 +123,6 @@ export default class CustomDocument extends Document {
             `,
             }}
           />
-
           <link rel="manifest" href="/manifest.json" />
           <link
             href="/icon/favicon-16x16.png"
