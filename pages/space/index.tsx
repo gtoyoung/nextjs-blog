568c752f6a1d8f19f56560d25d0d0837efdc5b89
ban <gtoyoung@naver.com>
2023-06-09 17:33:14 +0900

    NASA API 오류로 해당 페이지 수정

--- pages/space/index.tsx
+++ pages/space/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Layout } from "components/layout";
-import { NasaApi } from "services/nasa";
+// import { NasaApi } from "services/nasa";
 import { GetStaticProps } from "next";
-import NasaGallery from "components/nasa/gallery";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const api = new NasaApi();
-  const viewCnt: number = 10;
-  const reulst = await api.getNasaPicture(viewCnt);
-  const nasa = JSON.stringify(reulst);
+  // const api = new NasaApi();
+  // const viewCnt: number = 10;
+  // const reulst = await api.getNasaPicture(viewCnt);
+  const nasa = JSON.stringify({});
   return {
     props: {
       nasa,
@@ -16,17 +15,10 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-const Space = ({ nasa }) => {
-  const jsonConvert = JSON.parse(nasa);
-  return (
-    <Layout>
-      {nasa ? (
-        <NasaGallery key={"t"} pictures={jsonConvert} />
-      ) : (
-        <div>Loading...</div>
-      )}
-    </Layout>
-  );
+const Space = ({}) => {
+  // const jsonConvert = JSON.parse(nasa);
+  // return <Layout>{nasa ? <NasaGallery key={"t"} pictures={jsonConvert} /> : <div>Loading...</div>}</Layout>;
+  return <Layout>{<>API 문제 발생으로 인한 잠정 중단</>}</Layout>;
 };
 
 export default Space;
